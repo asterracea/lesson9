9cae07a306d3305e180e815e1ce97d3a3b383491
asterracea <alifwidya25@gmail.com>
2023-10-08 21:39:41 +0700

    menambahkan anotasi columnInfo pada variabel hex_color

--- app/src/main/java/com/example/lesson9/Color.kt
+++ app/src/main/java/com/example/lesson9/Color.kt
@@ -1,12 +1,13 @@
 package com.example.lesson9
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "colors")
 data class Color(
     @PrimaryKey(autoGenerate = true) val _id: Int,
-    val hex:String,
+    @ColumnInfo(name = "hex_color") val hex:String,
     val name:String
 )
 
