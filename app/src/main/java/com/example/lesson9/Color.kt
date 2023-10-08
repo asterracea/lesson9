85386108dd707e257825aa9417a0acc60a07bd94
asterracea <alifwidya25@gmail.com>
2023-10-08 21:38:07 +0700

    menambahkan anotasi primaryKey dan menambahkan variabel _id

--- app/src/main/java/com/example/lesson9/Color.kt
+++ app/src/main/java/com/example/lesson9/Color.kt
@@ -1,9 +1,11 @@
 package com.example.lesson9
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "colors")
 data class Color(
+    @PrimaryKey(autoGenerate = true) val _id: Int,
     val hex:String,
     val name:String
 )
