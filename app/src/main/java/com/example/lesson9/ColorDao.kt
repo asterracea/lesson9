0edda4ce3d4f5bd3e40b835a4ec844dd2ba57a56
asterracea <alifwidya25@gmail.com>
2023-10-08 21:49:07 +0700

    menambahkan query untuk menampilkan kolom name dalam tabel colors

--- app/src/main/java/com/example/lesson9/ColorDao.kt
+++ app/src/main/java/com/example/lesson9/ColorDao.kt
@@ -1,11 +1,16 @@
 package com.example.lesson9
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 
 @Dao
 interface ColorDao {
     @Query("SELECT * FROM colors")
     fun getAll(): Array<Color>
+    @Query("SELECT * FROM colors WHERE name = :name")
+    fun getColorByName(name: String): LiveData<Color>
+    
+
     @Insert
     fun insert(vararg color: Color)
     @Update
